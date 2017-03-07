367e1311a0515e5fea485aa20e6dab6fcb39fb9a
dennislam <lcyyyin24@gmail.com>
2017-03-07 11:52:50 +0800

    Dennis

--- app/src/main/java/com/example/dennislam/myapplication/AppliedJobGetDataTask.java
+++ app/src/main/java/com/example/dennislam/myapplication/AppliedJobGetDataTask.java
@@ -16,7 +16,7 @@ import java.util.List;
  */
 
 public class AppliedJobGetDataTask extends AsyncTask<Void,Void,Void> {
-
+//nvojgvskonvnsnnhnnghnhgh
     GlobalClass globalVariable;
 
     private AnimRFRecyclerView mRecyclerView;
