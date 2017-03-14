67a86129acdd41cf0a82d6080cea2f605521f303
on999you <k14k_lau@yahoo.com.hk>
2017-03-14 17:54:31 +0800

    translation, add layout and activity

--- app/src/main/java/com/example/dennislam/myapplication/activity/SettingsActivity.java
+++ app/src/main/java/com/example/dennislam/myapplication/activity/SettingsActivity.java
@@ -1,8 +1,21 @@
 package com.example.dennislam.myapplication.activity;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.example.dennislam.myapplication.R;
+
 /**
  * Created by shaunlau on 14/3/2017.
  */
 
 public class SettingsActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.activity_setting, null, false);
+        mDrawer.addView(contentView, 0);
+    }
 }
