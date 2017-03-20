c4905aee4b1e08464a771760bdadf1a7295700b1
on999you <k14k_lau@yahoo.com.hk>
2017-03-20 12:39:28 +0800

    improving lang func

--- app/src/main/java/com/example/dennislam/myapplication/activity/SettingsActivity.java
+++ app/src/main/java/com/example/dennislam/myapplication/activity/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.example.dennislam.myapplication.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,5 +18,9 @@ public class SettingsActivity extends BaseActivity{
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
         View contentView = inflater.inflate(R.layout.activity_setting, null, false);
         mDrawer.addView(contentView, 0);
+    }
+    public void passToLang(View v){
+        Intent intent = new Intent(getBaseContext(),LanguageActivity.class);
+        startActivity(intent);
     }
 }
