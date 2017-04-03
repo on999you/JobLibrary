5283ec6e37ca1dc1b0b194246f37058085af0dea
dennislam <lcyyyin24@gmail.com>
2017-04-03 11:34:43 +0800

    About us - email function

--- app/src/main/java/com/example/dennislam/myapplication/activity/AboutUsActivity.java
+++ app/src/main/java/com/example/dennislam/myapplication/activity/AboutUsActivity.java
@@ -1,5 +1,6 @@
 package com.example.dennislam.myapplication.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -15,5 +16,12 @@ public class AboutUsActivity extends BaseActivity {
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
         View contentView = inflater.inflate(R.layout.activity_about_us, null, false);
         mDrawer.addView(contentView, 0);
+    }
+
+    public void emailUs (View v) {
+        Intent email = new Intent(Intent.ACTION_SEND);
+        email.setType("message/rfc822");
+        email.putExtra(Intent.EXTRA_EMAIL, new String[]{"lcyyyin24@gmail.com"});
+        startActivity(Intent.createChooser(email, "Choose an Email client :"));
     }
 }
